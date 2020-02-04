5522bac7b37d1653b52fdc3469f1420f06221ec7
VLThomas11 <vthomas2@DMACC.edu>
2020-02-04 15:21:20 -0600

    update

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,9 +1,17 @@
+"""
+Program: coupon_calculations.py
+Author: Vickilee Thomas
+Last Modified Date 2/4/2020
+
+The purpose of this program is to calculate the total cost a customer owes after discounts, taxes, and shipping costs.
+"""
+
+
+
 def calculate_price(price, cash_coupon, percent_coupon):
     percent_discount_multiplier = (100 - percent_coupon) / 100
-    print(percent_discount_multiplier)
 
     final_price = (price - cash_coupon) * percent_discount_multiplier
-    print(final_price)
 
     if final_price < 10:
         shipping = 5.95
@@ -13,21 +21,20 @@ def calculate_price(price, cash_coupon, percent_coupon):
         shipping = 11.95
     elif final_price >= 50:
         shipping = 0
-    print(shipping)
 
     tax = final_price * .06
-    final_plus_tax = tax + final_price
-    total = final_plus_tax + shipping
-    print(tax)
-    print(final_plus_tax)
-    print(total)
+    total = final_price + tax + shipping
+
+    return total
 
 
 def customer_order():
     price = float(input("What is the price of your item? "))
     cash_coupon = int(input("How much is your cash coupon? "))
     percent_coupon = int(input("How much is your percent coupon? "))
-    calculate_price(price, cash_coupon, percent_coupon)
+    total_cost = calculate_price(price, cash_coupon, percent_coupon)
+    rounded_total = round(total_cost, 2)
+    print("The you total cost is $",rounded_total)
 
 
 if __name__ == '__main__':
