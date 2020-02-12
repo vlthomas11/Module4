c23e3366a4d18aa0f9020b6fefa0d3b68b9de45e
VLThomas11 <vthomas2@DMACC.edu>
2020-02-12 15:16:50 -0600

    update

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -1,20 +1,34 @@
 def average(score1, score2, score3):
+    try:
+        if score1 < 0:
+            raise ValueError
+    except ValueError:
+        print("You did not enter a positive number")
     NUMBER_TESTS = 3
     averageScore = float((score1 + score2 + score3) / NUMBER_TESTS)
-    print(averageScore)
     return float((score1 + score2 + score3) / NUMBER_TESTS)
 
 
 def run_average():
     score1 = int(input("Enter the first score "))
     try:
-        if score1 > 0:
+        if score1 < 0:
             raise ValueError
-    except:
-        print("your value was less than 0")
-        # score1  =  int(input("You did not enter a positive number, please try again "))
-    score2 = int(input("Enter the second score "))
-    score3 = int(input("Enter the third score "))
+    except ValueError:
+        score1 = int(input("You did not enter a positive number, please try again "))
+    score2 = int(input("Enter the first score "))
+    try:
+        if score2 < 0:
+            raise ValueError
+    except ValueError:
+        score2 = int(input("You did not enter a positive number, please try again "))
+    score3 = int(input("Enter the first score "))
+    try:
+        if score3 < 0:
+            raise ValueError
+    except ValueError:
+        score3 = int(input("You did not enter a positive number, please try again "))
+
     avgScore = average(score1, score2, score3)
     print(avgScore)
 
--- test/test_average_negative_input.py
+++ test/test_average_negative_input.py
@@ -1,7 +1,11 @@
 import unittest
 from input_validation import validation_with_try as topic4
 
+
 def test_average_exception(self):
     with self.assertRaises(ValueError):
         topic4.average(-90, 89, 78)
 
+
+if __name__ == '__main__':
+    unittest.main()
